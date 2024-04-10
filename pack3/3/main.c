25c10aa837304853d08656229fad0a6ce8e07098
Ilya <anzhinilya2@gmail.com>
2024-04-10 22:17:15 +0700

    maps file read

--- pack3/3/main.c
+++ pack3/3/main.c
@@ -3,9 +3,85 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define PAGE_SIZE 4096
 #define BUFF_SIZE 256
+
+typedef struct page {
+    uint64_t start_addr;
+    uint64_t end_addr;
+} page;
+
+typedef struct page_array {
+    page *array;
+    int size;
+} page_array;
+
+void clear_page_array(page_array *pg_array) {
+    if (pg_array->array != NULL) {
+        free(pg_array->array);
+    }
+}
+
+int get_mem_range(char *maps_filename, page_array *pg_array) {
+    int capacity = 128;
+    int curr_size = 0;
+    pg_array->array = malloc(sizeof(page) * capacity);
+
+    if (pg_array->array == NULL) {
+        perror("can't allocate enough memory");
+        return -1;
+    }
+
+    int buff_size = 512;
+    char buff[buff_size];
+    FILE *file = fopen(maps_filename, "r");
+
+    if (file == NULL) {
+        perror("Couldn't open maps file");
+        return -1;
+    }
+
+    while (fgets(buff, buff_size, file) != NULL) {
+        char *space_pt = strchr(buff, ' ');
+        if (space_pt == NULL) {
+            return -1;
+        }
+        char *del_pt = strchr(buff, '-');
+        if (del_pt == NULL) {
+            return -1;
+        }
+
+        buff[del_pt - &buff[0]] = '\000';
+        buff[space_pt - &buff[0]] = '\000';
+        int errno = 0;
+        uint64_t start_addr = strtol(buff, NULL, 16);
+        uint64_t end_addr = strtol(del_pt + 1, NULL, 16);
+
+        if (errno != 0) {
+            perror("Couldn't parse maps file");
+            return  -1;
+        }
+
+        page new_page = {start_addr, end_addr};
+        if (capacity <= curr_size) {
+            capacity *= 2;
+            page *tmp = realloc(pg_array->array, capacity * sizeof(page));
+            if (tmp == NULL) {
+                perror("can't allocate enough memory");
+                return -1;
+            }
+            pg_array->array = tmp;
+        }
+
+        pg_array->array[curr_size] = new_page;
+        curr_size++;
+
+    }
+    pg_array->size = curr_size;
+    return 0;
+}
 
 int inspect_file(int fd, uint64_t start_addr, uint64_t end_addr) {
     for (uint64_t i = start_addr; i <= end_addr; i += PAGE_SIZE) {
@@ -32,32 +108,45 @@ int inspect_file(int fd, uint64_t start_addr, uint64_t end_addr) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3 && argc != 4) {
-        printf("Usage: %s pid, start addr, end addr\nor\n%s pid, addr\n     ", argv[0], argv[0]);
+    if (argc != 2) {
+        printf("Usage:\n%s pid\n     ", argv[0]);
         return 0;
     }
 
     int errno = 0;
     int pid = (int) strtol(argv[1], NULL, 10);
-    uint64_t start_addr = strtol(argv[2], NULL, 16);
-    uint64_t end_addr = argc == 3 ? start_addr : strtol(argv[3], NULL, 16);
-
     if (errno != 0) {
         printf("invalid arguments\n");
         return -1;
     }
 
     char filename[BUFF_SIZE];
+    char maps_path[BUFF_SIZE];
     snprintf(filename, BUFF_SIZE, "/proc/%d/pagemap", pid);
+    snprintf(maps_path, BUFF_SIZE, "/proc/%d/maps", pid);
 
-    int fd = open(filename, O_RDONLY);
+    int fd_pagemap = open(filename, O_RDONLY);
 
-    if (fd < 0) {
+    if (fd_pagemap < 0) {
         perror("Couldn't open pagemap file");
+        close(fd_pagemap);
         return -1;
     }
 
-    int res_code = inspect_file(fd, start_addr, end_addr);
-    close(fd);
-    return res_code;
+    page_array pg_array;
+    int res_code = get_mem_range(maps_path, &pg_array);
+    if (res_code != 0) {
+        clear_page_array(&pg_array);
+        return -1;
+    }
+    for (int i = 0; i < pg_array.size; i++) {
+        res_code = inspect_file(fd_pagemap, pg_array.array[i].start_addr, pg_array.array[i].end_addr);
+        if (res_code != 0) {
+            close(fd_pagemap);
+            clear_page_array(&pg_array);
+            return -1;
+        }
+    }
+    clear_page_array(&pg_array);
+    close(fd_pagemap);
 }
