436dacc1e9933a548a3848a65d33837029bde1be
Ilya <anzhinilya2@gmail.com>
2024-05-14 14:38:05 +0700

    update

--- pack6/1/main.c
+++ pack6/1/main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <stdbool.h>
-#include <fcntl.h>
 #include <unistd.h>
 
 #define PAGE_SIZE 4096
 const char *shared_file = "shared";
 
-void writer() {
-    int shared_fd = open(shared_file, O_RDWR | O_CREAT, 0660);
-    ftruncate(shared_fd, 0);
-    ftruncate(shared_fd, PAGE_SIZE);
-    unsigned int *memory = mmap(NULL, PAGE_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, shared_fd, 0);
-    close(shared_fd);
-    int num = PAGE_SIZE / sizeof(unsigned int);
+void writer(unsigned int *memory, int num) {
+//    int shared_fd = open(shared_file, O_RDWR | O_CREAT, 0660);
+//    ftruncate(shared_fd, 0);
+//    ftruncate(shared_fd, PAGE_SIZE);
+//    unsigned int *memory = mmap(NULL, PAGE_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, shared_fd, 0);
+//    close(shared_fd);
+//    int num = PAGE_SIZE / sizeof(unsigned int);
 
     unsigned int curr_num = 0;
     while (true) {
@@ -26,12 +25,12 @@ void writer() {
 
 }
 
-void reader() {
+void reader(unsigned int *memory, int num) {
     sleep(1);
-    int shared_fd = open(shared_file, O_RDWR | O_CREAT, 0660);
-    unsigned int *memory = mmap(NULL, PAGE_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, shared_fd, 0);
-    close(shared_fd);
-    int num = PAGE_SIZE / sizeof(unsigned int);
+//    int shared_fd = open(shared_file, O_RDWR | O_CREAT, 0660);
+//    unsigned int *memory = mmap(NULL, PAGE_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, shared_fd, 0);
+//    close(shared_fd);
+//    int num = PAGE_SIZE / sizeof(unsigned int);
 
     unsigned int prev_read = memory[0];
     unsigned int curr_read;
@@ -51,15 +50,18 @@ void reader() {
 }
 
 int main() {
+    unsigned int *memory = mmap(NULL, PAGE_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    int num = PAGE_SIZE / sizeof(unsigned int);
     int pid = fork();
+
 
     if (pid < 0) {
         return -1;
     }
 
     if (pid > 0) {
-        writer();
+        writer(memory, num);
     } else {
-        reader();
+        reader(memory, num);
     }
 }
