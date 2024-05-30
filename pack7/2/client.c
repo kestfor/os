08362fcd4a490a3e2e646e65222350973f0c4fe3
Ilya <anzhinilya2@gmail.com>
2024-05-30 19:12:41 +0700

    7.2

--- pack7/2/client.c
+++ pack7/2/client.c
@@ -20,7 +20,7 @@ int main() {
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(82);
+    server_addr.sin_port = htons(5000);
     inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
     bzero(&(server_addr.sin_zero),8);
 
--- pack7/2/server.c
+++ pack7/2/server.c
@@ -51,7 +51,7 @@ int main() {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(82);
+    server_addr.sin_port = htons(5000);
     bzero(&(server_addr.sin_zero),8);
 
     int err = bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
