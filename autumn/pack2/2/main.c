83ab318063f4ac5ff18da09bdadf19e725de4f86
Ilya <anzhinilya2@gmail.com>
2024-10-07 13:45:11 +0700

    deadlock fix

--- autumn/pack2/2/main.c
+++ autumn/pack2/2/main.c
@@ -13,6 +13,8 @@ int second_counter = 0;
 int third_counter = 0;
 int fourth_counter = 0;
 
+pthread_mutex_t counter_mutex;
+
 void *first_thread(void *arg) {
     list *l = (list *) arg;
     while (true) {
@@ -103,24 +105,32 @@ void *fourth_thread(void *arg) {
 
             if (is_perm_need()) {
                 if (prev != NULL) {
-                    pthread_mutex_lock(&prev->sync);
                     prev->next = next;
-                    pthread_mutex_unlock(&prev->sync);
+//                    pthread_mutex_unlock(&prev->sync);
                 }
+                node *old_prev = prev;
                 curr->next = next->next;
                 next->next = curr;
                 prev = next;
                 next = curr->next;
+                pthread_mutex_lock(&counter_mutex);
                 fourth_counter++;
-                pthread_mutex_unlock(&next->sync);
+                pthread_mutex_unlock(&counter_mutex);
+                if (old_prev != NULL) {
+                    pthread_mutex_unlock(&old_prev->sync);
+                }
             } else {
+                node *old_prev = prev;
                 prev = curr;
                 curr = next;
                 next = curr->next;
-                pthread_mutex_unlock(&prev->sync);
+                if (old_prev != NULL) {
+                    pthread_mutex_unlock(&old_prev->sync);
+                }
             }
         }
         pthread_mutex_unlock(&curr->sync);
+        pthread_mutex_unlock(&prev->sync);
 
     }
 }
@@ -140,15 +150,16 @@ int main() {
     pthread_create(&first, NULL, first_thread, l);
     pthread_create(&second, NULL, second_thread, l);
     pthread_create(&third, NULL, third_thread, l);
-//
-//    for (int i = 0; i < 3; i++) {
-//        pthread_create(&perm[i], NULL, fourth_thread, l);
-//    }
+
+
+    pthread_mutex_init(&counter_mutex, NULL);
+    for (int i = 0; i < 3; i++) {
+        pthread_create(&perm[i], NULL, fourth_thread, l);
+    }
 
     while (true) {
         sleep(1);
         printf("%d, %d, %d, %d\n", first_counter, second_counter, third_counter, fourth_counter);
-
     }
 
     clear(l);
